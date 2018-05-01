bde5ef2a6867a024b9375078031885c37e565f9a
Anryan <2665572581@qq.com>
2018-05-01 11:43:36 +0800

    Add files via upload

---
+++ test_exe.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int test(){
+	int a=1,b=2;
+	return a+b;
+}
+
+int main(){
+	cout<<"strat to test the exe_file"<<endl;
+	int num=test();
+	cout<<"the test result is :"<<num<<endl;
+	cout<<"end the test "<<endl;
+}
